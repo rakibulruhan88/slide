1829be04bb4266b6507b622edef13b59166272a1
unknown <mdrohan7224@gmail.com>
2023-12-06 00:27:31 +0600

    slide

---
+++ script.js
@@ -0,0 +1,15 @@
+let menu = document.getElementById ("manu");
+
+let nav = document.getElementById("nav-bar");
+let cross = document.getElementById ("cross")
+
+function slide () {
+    nav.style.display ="flex";
+    cross.style.display = "flex"
+}
+
+console.log (nav);
+
+function crossx () {
+    nav.style.display = ("none");
+}
